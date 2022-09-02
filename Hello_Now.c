96bc61994726911575eb6e398e819f67d6da8871
Leoluwa <leoluamid@gmail.com>
2022-09-01 23:58:22 -0700

    Alhamdullilahj

---
+++ Hello_Now.c
@@ -0,0 +1,13 @@
+
+/* I am so glad to be doing software engineering :) */
+
+#include "stdio.h"
+#include "stdlib.h"
+
+
+int main( void )
+{
+  printf( "\n\n\t Creating future\n\n" );
+
+  return( 0 );
+}
